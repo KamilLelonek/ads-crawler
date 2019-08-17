353b659c0f3a154a4808582e8c39002b5f70a804
Kamil Lelonek <squixy.sln@gmail.com>
2019-08-17 13:55:14 +0200

    Ensure DB schema on start

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "crawler/config"
+import (
+	"crawler/config"
+	"crawler/publisher"
+)
 
 func main() {
 	router := config.SetupRouter()
+	db := config.SetupDB()
 
+	publisher.Migrate(db)
+
+	defer db.Close()
 	router.Run()
 }
